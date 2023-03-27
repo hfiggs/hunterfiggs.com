9acef61a65b10617c86cb2b2701da00f6e8731dd
Hunter Figgs <hunter.figgs@gmail.com>
2023-03-27 14:07:53 -0400

    Add predict-digit API endpoint.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,30 @@
-from fastapi import FastAPI
+import cv2 # type: ignore
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import HTMLResponse
+import numpy as np
+
+import app.mnist_tools.model # type: ignore
+
+model = app.mnist_tools.model.MnistModel()
 
 app = FastAPI()
 
-
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def main():
+    content = """
+<body>
+<form action="/predict-digit/" enctype="multipart/form-data" method="post">
+<input name="file" type="file">
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
 
+@app.post("/predict-digit/")
+async def predict_digit(file: UploadFile):
+    img_raw = await file.read()
+    img_buf = np.asarray(bytearray(img_raw), dtype="uint8")
+    img = cv2.imdecode(img_buf, cv2.IMREAD_UNCHANGED)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+    return {"probs": model.predict_probs(img)}
