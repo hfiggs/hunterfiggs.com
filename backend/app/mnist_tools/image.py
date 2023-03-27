06b4d07b7e24703847f6903f6074f1993133cd01
Hunter Figgs <hunter.figgs@gmail.com>
2023-03-27 14:07:02 -0400

    Add mnist_tools package to backend app

---
+++ backend/app/mnist_tools/image.py
@@ -0,0 +1,36 @@
+import math
+
+import cv2 # type: ignore
+import numpy as np
+
+from app.mnist_tools.config import MNIST_IMAGE_SIZE, MNIST_IMAGE_BORDER_SIZE, MNIST_IMAGE_BORDER_VALUE, MNIST_IMAGE_WITH_BORDER_SIZE, MNIST_IMAGE_WITH_BORDER_AREA # type: ignore
+
+def preprocess(img: cv2.Mat) -> cv2.Mat:
+    # If image dimensions are invalid, return empty image of correct size
+    if (not hasattr(img, 'shape')) or len(img.shape) != 3 or img.shape[0] == 0 or img.shape[1] == 0 or (img.shape[2] != 3 and img.shape[2] != 4):
+        return cv2.Mat(np.zeros((MNIST_IMAGE_WITH_BORDER_SIZE, MNIST_IMAGE_WITH_BORDER_SIZE), dtype='uint8'))
+
+    gray_img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2GRAY)
+
+    th = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    coords = cv2.findNonZero(th)
+    x,y,w,h = cv2.boundingRect(coords)
+
+    cropped_img = gray_img[y:y+h, x:x+w]
+
+    old_shape = cropped_img.shape
+    if old_shape[0] > old_shape[1]:
+        new_shape = (int((MNIST_IMAGE_SIZE / old_shape[0]) * old_shape[1]), MNIST_IMAGE_SIZE)
+    else:
+        new_shape = (MNIST_IMAGE_SIZE, int((MNIST_IMAGE_SIZE / old_shape[1]) * old_shape[0]))
+
+    normalized_img = cv2.resize(cropped_img, new_shape, interpolation=cv2.INTER_AREA)
+
+    v_padding: float = MNIST_IMAGE_BORDER_SIZE + (0 if (normalized_img.shape[0] == MNIST_IMAGE_SIZE) else (MNIST_IMAGE_SIZE - normalized_img.shape[0])/2)
+    h_padding: float = MNIST_IMAGE_BORDER_SIZE + (0 if (normalized_img.shape[1] == MNIST_IMAGE_SIZE) else (MNIST_IMAGE_SIZE - normalized_img.shape[1])/2)
+ 
+    padded_img = cv2.copyMakeBorder(normalized_img, math.ceil(v_padding), math.floor(v_padding), math.ceil(h_padding), math.floor(h_padding), cv2.BORDER_CONSTANT, value=MNIST_IMAGE_BORDER_VALUE)
+
+    inverted_img = cv2.bitwise_not(padded_img)
+
+    return inverted_img
---
+++ backend/app/mnist_tools/model.py
@@ -0,0 +1,44 @@
+import importlib.resources
+import pickle
+
+import cv2 # type: ignore
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier # type: ignore
+
+from app.mnist_tools.config import MNIST_IMAGE_WITH_BORDER_AREA # type: ignore
+import app.mnist_tools.image # type: ignore
+import app.mnist_tools.loader # type: ignore
+
+_BIN_PATH = 'app.mnist_tools.bin'
+_MODEL_BIN_FILE_NAME = 'knn_model_bin.pkl'
+
+_KNN_NUM_NEIGHBORS: int = 3
+
+class MnistModel:
+    def __init__(self) -> None:
+
+        with importlib.resources.as_file(importlib.resources.files(_BIN_PATH)) as bin_path:
+
+            if (bin_path / _MODEL_BIN_FILE_NAME).is_file():
+                with open(str(bin_path / _MODEL_BIN_FILE_NAME), 'rb') as model_bin_file:
+                    self.model = pickle.load(model_bin_file)
+
+                    if not isinstance(self.model, KNeighborsClassifier):
+                        raise ValueError(f"Model loaded from file has incorrect type, expected {str(KNeighborsClassifier)}, got {str(type(self.model))}")
+
+            else:
+                self.model = KNeighborsClassifier(n_neighbors=_KNN_NUM_NEIGHBORS)
+                train_images, train_labels, _, _ = app.mnist_tools.loader.load()
+                num_samples, num_x, num_y = train_images.shape
+                train_images = train_images.reshape(num_samples, num_x*num_y)
+                self.model.fit(train_images, train_labels)
+                with open(str(bin_path / _MODEL_BIN_FILE_NAME), 'wb') as model_bin_file:
+                    pickle.dump(self.model, model_bin_file)
+
+
+    def predict_probs(self, img: cv2.Mat) -> dict[int, float]:
+        img = app.mnist_tools.image.preprocess(img)
+
+        flat_img = np.array(img).reshape(1, MNIST_IMAGE_WITH_BORDER_AREA)
+
+        return dict(enumerate(self.model.predict_proba(flat_img)[0]))
