ad9a9440453dfabd0a32133af1807fe8124da6d6
Hunter Figgs <hunter.figgs@gmail.com>
2023-03-28 15:39:58 -0400

    Remove mobile jump script

--- frontend/scripts/background.js
+++
@@ -1,9 +0,0 @@
-var bg = document.getElementById("background");
-
-function resizeBackground() {
-  bg.style.height = window.innerHeight + 60;
-}
-
-window.addEventListener("resize", resizeBackground, true);
-
-resizeBackground();
