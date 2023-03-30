6a715e92ad6ad59d57db1068608c008ec8b545ec
Hunter Figgs <hunter.figgs@gmail.com>
2023-03-30 17:47:31 -0400

    Add support for touch to digit detector

--- frontend/projects/detect-digit/scripts/draw.js
+++ frontend/projects/detect-digit/scripts/draw.js
@@ -5,14 +5,14 @@ var resultContext = resultCanvas.getContext("2d");
 
 var x = 0;
 var y = 0;
-var offsetX;
-var offsetY;
 var isMouseDown = false;
 
 const CANVAS_BACKGROUND_FILL_STYLE = "white";
 const DRAW_STROKE_STYLE = "black";
 const DRAW_LINE_JOIN = "round"
 const DRAW_LINE_WIDTH = 14;
+
+// Handle MOUSE interactions
 
 drawCanvas.addEventListener("mousedown", onMouseDown);
 drawCanvas.addEventListener("mousemove", onMouseMove);
@@ -39,6 +39,74 @@ function onMouseUp(e) {
     x = 0;
     y = 0;
   }
+}
+
+// Handle TOUCH interactions
+
+var drawCanvasX; // Relative to viewport
+var drawCanvasY; // Relative to viewport
+var currentTouches = [];
+
+drawCanvas.addEventListener("touchstart", onTouchStart);
+drawCanvas.addEventListener("touchmove", onTouchMove);
+drawCanvas.addEventListener("touchend", onTouchEnd);
+drawCanvas.addEventListener("touchcancel", onTouchCancel);
+
+function onTouchStart(e) {
+  if (e.cancelable) e.preventDefault();
+
+  drawCanvasX = drawCanvas.getBoundingClientRect().left;
+  drawCanvasY = drawCanvas.getBoundingClientRect().top;
+
+  for (let i = 0; i < e.changedTouches.length; i++) {
+    currentTouches.push(copyTouch(e.changedTouches[i]));
+  }
+}
+
+function onTouchMove(e) {
+  if (e.cancelable) e.preventDefault();
+
+  for (let i = 0; i < e.changedTouches.length; i++) {
+
+    let touchIdx = currentTouchIndexById(currentTouches, e.changedTouches[i].identifier);
+
+    if (touchIdx >= 0) {
+      drawLine(drawContext, currentTouches[touchIdx].clientX - drawCanvasX, currentTouches[touchIdx].clientY - drawCanvasY, e.changedTouches[i].clientX - drawCanvasX, e.changedTouches[i].clientY - drawCanvasY);
+      currentTouches.splice(touchIdx, 1, copyTouch(e.changedTouches[i])); // replace with latest touch
+    }
+  }
+}
+
+function onTouchEnd(e) {
+  if (e.cancelable) e.preventDefault();
+
+  for (let i = 0; i < e.changedTouches.length; i++) {
+
+    let touchIdx = currentTouchIndexById(currentTouches, e.changedTouches[i].identifier);
+
+    if (touchIdx >= 0) {
+      currentTouches.splice(touchIdx, 1);
+    }
+  }
+}
+
+function onTouchCancel(e) {
+  onTouchEnd(e);
+}
+
+function copyTouch({ identifier, clientX, clientY }) {
+  return { identifier, clientX, clientY };
+}
+
+function currentTouchIndexById(currentTouches, idToFind) {
+  for (let i = 0; i < currentTouches.length; i++) {
+    const id = currentTouches[i].identifier;
+
+    if (id === idToFind) {
+      return i;
+    }
+  }
+  return -1; // id not found
 }
 
 function drawLine(context, x1, y1, x2, y2) {
